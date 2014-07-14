a13f8d3aa63e5685c2f5fadc31ca0e48c9bcb86f
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-14 22:42:27 +0530

    updating

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-
 /**
  * Module dependencies.
  */
 
-var Builder = require('component-builder')
-  , templates = require('./templates')
-  , rework = require('./rework')
-  , fs = require('fs')
-  , write = fs.writeFileSync
+var Builder = require('component-builder'),
+    templates = require('./templates'),
+    rework = require('./rework'),
+    fs = require('fs'),
+    write = fs.writeFileSync,
+    env = process.env.NODE_ENV;
 
 /**
  * Component builder middleware.
  */
 
-module.exports = function(req, res, next){
-  var builder = new Builder('.');
-  builder.addLookup('app'); // TODO: shouldn't be necessary
-  builder.copyAssetsTo('public');
-  builder.use(rework);
-  builder.use(templates);
-  builder.build(function(err, res){
-    if (err) return next(err);
-    write('public/app.js', res.require + res.js);
-    write('public/app.css', res.css);
-    next();
-  });
+module.exports = function (req, res, next) {
+    if (env === 'production') {
+        return next();
+    }
+    var builder = new Builder('.');
+    builder.addLookup('app'); // TODO: shouldn't be necessary
+    builder.copyAssetsTo('public');
+    builder.use(rework);
+    builder.use(templates);
+    builder.build(function (err, res) {
+        if (err) return next(err);
+        write('public/app.js', res.require + res.js);
+        write('public/app.css', res.css);
+        next();
+    });
 };
